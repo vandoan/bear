216adacb3be65eefda89461cfb1aa9a6dade0d0a
Van Doan <vdoan09@gmail.com>
2014-07-19 00:34:46 -0400

    Added images ability, additional features to the form page, stylized the home page, and added a Google Map at the bottom of the index page.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ActiveRecord::Base
+
+	mount_uploader :image, ImageUploader
+#	mount_uploader :attachment, AttachmentUploader
+#	geocoded_by :location 		# can also be an ip address
+#	after_validation :geocode 	# auto-fetch coordinates 
 end
